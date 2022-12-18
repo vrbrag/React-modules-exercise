f2598847abd3d8475e4d8ac8d163bc142b9ae8b5
Vanessa Bragdon <vrbragdon@gmail.com>
2022-12-17 19:55:10 -0800

    commit fruits

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import fruits from './fruits'
+import { choice, remove } from './helpers'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let randomFruit = choice(fruits)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(`I'd like one ${randomFruit}, please.`)
+console.log(`Here you go: ${randomFruit}.`)
+console.log(`Delicious! May I have another?`)
+
+let remaining = remove(randomFruit, fruits)
+
+console.log(`I'm sorry we're all out. We have ${remaining.length} left. `)
